24d6e3a16f4fbd4c2db1b1cb9fe3a0fa75cbb75c
Madison Evans <m.corbinevans@gmail.com>
2023-03-28 13:19:34 -0700

    updated dummy data

--- client/src/Components/List.jsx
+++ client/src/Components/List.jsx
@@ -1,5 +1,5 @@
 import jobs from "../DummyData/jobs.json";
-
+import companies from "../DummyData/companies.json";
 export default function Example() {
 	function showJobDetails(e) {
 		// show job details logic goes here
@@ -7,12 +7,14 @@ export default function Example() {
 	}
 	return (
 		<ul className="divide-y divide-gray-200">
-			{jobs.map((job) => (
-				<li key={job.role} className="flex py-4">
+			{jobs.map((job, id) => (
+				<li key={id} className="flex py-4">
 					<div
 						className="ml-3 cursor-pointer"
 						onClick={(e) => showJobDetails(e)}>
-						<p className="text-sm font-medium text-gray-900">company_name</p>
+						<p className="text-sm font-medium text-gray-900">
+							{companies[id].companyName}
+						</p>
 						<p className="text-sm text-gray-500">{job.role}</p>
 					</div>
 				</li>
