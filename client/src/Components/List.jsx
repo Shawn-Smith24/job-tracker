d1377a4c763f5409d8bcbfa183127bf52f4f477a
Madison Evans <m.corbinevans@gmail.com>
2023-03-28 13:01:16 -0700

    adding list to dashboard

--- client/src/Components/List.jsx
+++ client/src/Components/List.jsx
@@ -1,24 +1,17 @@
-const jobs = [
-	{
-		companyName: "Apple",
-		role: "Frontend Engineer",
-	},
-	{
-		companyName: "Google",
-		role: "Backend Engineer",
-	},
-	{
-		companyName: "Netflix",
-		role: "Full-Stack Engineer",
-	},
-];
+import jobs from "../DummyData/jobs.json";
 
 export default function Example() {
+	function showJobDetails(e) {
+		// show job details logic goes here
+		console.log(`showing Job Details for ${e.target}`);
+	}
 	return (
 		<ul className="divide-y divide-gray-200">
 			{jobs.map((job) => (
 				<li key={job.role} className="flex py-4">
-					<div className="ml-3">
+					<div
+						className="ml-3 cursor-pointer"
+						onClick={(e) => showJobDetails(e)}>
 						<p className="text-sm font-medium text-gray-900">
 							{job.companyName}
 						</p>
