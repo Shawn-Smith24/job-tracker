54bfd1d2728411c054dcbaa2acea26500470197d
Madison Evans <m.corbinevans@gmail.com>
2023-03-28 14:08:58 -0700

    building Navigation

--- client/src/Components/Navigation.jsx
+++ client/src/Components/Navigation.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Navigation = () => {
-	return <div className="bg-white h-16">Navigation</div>;
+	return (
+		<div className="relative h-16 flex flex-row justify-center shadow-lg">
+			<input
+				type="text"
+				className="w-[300px] my-3 rounded-sm bg-info text-primary px-4"
+			/>
+			<div className="absolute right-0">asdf</div>
+		</div>
+	);
 };
 
 export default Navigation;
