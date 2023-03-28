d590b6d77b8173841de902cb8e70978f09cc6e6b
Madison Evans <m.corbinevans@gmail.com>
2023-03-28 14:16:22 -0700

    building Navigation

--- client/src/Components/Navigation.jsx
+++ client/src/Components/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { RiLogoutBoxRFill as Logout } from "react-icons/ri";
+import { Link } from "react-router-dom";
 const Navigation = () => {
 	return (
 		<div className="relative h-16 flex flex-row justify-center shadow-lg">
@@ -7,7 +8,11 @@ const Navigation = () => {
 				type="text"
 				className="w-[300px] my-3 rounded-sm bg-info text-primary px-4"
 			/>
-			<div className="absolute right-0">asdf</div>
+			<div className="cursor-pointer absolute right-0 w-fit pr-6 text-info top-1/2 -translate-y-1/2">
+				<Link to="/signin">
+					<Logout color="#F0F0F0" size="2em" />
+				</Link>
+			</div>
 		</div>
 	);
 };
