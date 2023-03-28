42655966dd32c3c96f8cd73ce918baf9aa76fa54
Madison Evans <m.corbinevans@gmail.com>
2023-03-28 13:37:19 -0700

    updating job detail and application card

--- client/src/Components/JobDetailSection.jsx
+++ client/src/Components/JobDetailSection.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import companies from "../DummyData/companies.json";
+import jobs from "../DummyData/jobs.json";
 
 const JobDetailSection = () => {
 	return (
@@ -6,16 +8,18 @@ const JobDetailSection = () => {
 			{applicationCard()}
 		</div>
 	);
-
 	function applicationCard() {
+		const experience = ["Apprentice", "Jr", "Mid-Level", "Sr"];
 		return (
 			<div className="absolute w-4/5 full h-4/5 bg-accent p-8">
-				<h2 className="font-display text-6xl w-full text-center">role</h2>
+				<h2 className="font-display text-6xl w-full text-center">
+					{jobs[0].role}
+				</h2>
 				<ul className="border">
-					<li>company_name</li>
-					<li>location</li>
-					<li>salary</li>
-					<li>experience_level</li>
+					<li>{companies[0].companyName}</li>
+					<li>{jobs[0].location}</li>
+					<li>{jobs[0].salary}</li>
+					<li>{experience[jobs[0].expLevel]}</li>
 				</ul>
 			</div>
 		);
