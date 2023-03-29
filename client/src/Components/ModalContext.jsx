1b493bd17d70f53703a01b92a298e683b8990f57
Madison Evans <m.corbinevans@gmail.com>
2023-03-28 20:38:33 -0700

    working on modal

--- client/src/Components/ModalContext.jsx
+++ client/src/Components/ModalContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 const ModalContext = createContext();
 
 export const ModalProvider = ({ children }) => {
-	const [showModal, setShowModal] = useState(true);
+	const [showModal, setShowModal] = useState(false);
 
 	return (
 		<ModalContext.Provider value={{ showModal, setShowModal }}>
