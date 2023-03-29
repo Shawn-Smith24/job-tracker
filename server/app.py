a96d8df9cac0758d5bb2b66f59d06eaf3520cf1d
shawn <shawnsmith0024@gmail.com>
2023-03-29 09:41:59 -0400

    db set up with test seed data, relationships working

--- server/app.py
+++ server/app.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python3
-
-# Standard library imports
-from flask import Flask
+from flask import Flask, make_response, request, jsonify
 from flask_migrate import Migrate
-# Remote library imports
-from flask import request
-from flask_restful import Resource
+from flask_restful import Api, Resource
 
-# Local imports
-from config import app, db, api
-from models import Job, Company, Application, User
+from models import db, Job, User, Application, Company
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -20,7 +13,11 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-# api = api(app)
+api = Api(app)
+
+@app.route('/')
+def home():
+    return ''
 
 # user1= User(user_id= 1, username='test', password='test')
 # job1= Job(job_id = 1, job_name='test', location='test', salary=100, experience_level=1)
--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+from config import db
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
@@ -11,67 +12,69 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-from config import db
-
 # Models go here!
 
+
 class User(db.Model, SerializerMixin):
-    
-    
     __tablename__ = 'users'
-    
+
+    serialize_rules = ('-application.user_id',)
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
     password = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())    
-    
-    # applications = db.relationship('Applications', backref='user')
-    
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    applied = db.relationship('Application', backref='user')
+
+
 class Application(db.Model, SerializerMixin):
-    
+
     __tablename__ = 'applications'
-    
+
     id = db.Column(db.Integer, primary_key=True)
-    status= db.Column(db.Integer)
+    status = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now()) 
-    
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     job_id = db.Column(db.Integer, db.ForeignKey('jobs.id'))
+
     
-    user = db.relationship('Users', backref=('applications'))
-    job= db.relationship('Jobs', backref=('applications'))
-    
+    job = db.relationship('Job', backref='application')
+
+
 class Job(db.Model, SerializerMixin):
-    
+
     __tablename__ = 'jobs'
-    
-    
+
+    serialize_rules = ('-application.job_id', '-company.company.id')
+
     id = db.Column(db.Integer, primary_key=True)
-    job_name= db.Column(db.String)
+    job_name = db.Column(db.String)
     location = db.Column(db.String)
     salary = db.Column(db.Integer)
-    experience_level= db.Column(db.Integer)
+    experience_level = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now()) 
-    
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
     company_id = db.Column(db.Integer, db.ForeignKey('companies.id'))
-   
-   #relationships 
-    applications = db.relationship('Applications', backref='job')
-    company= db.relationship('Companies', backref='jobs')
-    
-    
+
+   # relationships
+    # applications = db.relationship('Application', backref='job')
+    company = db.relationship('Company', backref='job')
+
+
 class Company(db.Model, SerializerMixin):
-    
+
     __tablename__ = 'companies'
-    
-    
+
+    serialize_rules = ('-job.company_id',)
+
     id = db.Column(db.Integer, primary_key=True)
     company_name = db.Column(db.String)
     company_bio = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now()) 
-    
-    company= db.relationship('Company', backref='jobs')
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
