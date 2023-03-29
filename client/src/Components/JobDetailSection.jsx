9f023815075d6297ce1919149255bab74602e800
Madison Evans <m.corbinevans@gmail.com>
2023-03-28 18:52:16 -0700

    styling updates

--- client/src/Components/JobDetailSection.jsx
+++ client/src/Components/JobDetailSection.jsx
@@ -16,14 +16,17 @@ const JobDetailSection = () => {
 	function applicationCard() {
 		const experience = ["Apprentice", "Jr", "Mid-Level", "Sr"];
 		return (
-			<div className="absolute w-4/5 full h-[600px] p-8 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 bg-primary rounded-xl border border-secondary shadow-lg">
+			<div
+				className="
+				absolute w-4/5 max-w-[650px] full h-[600px] p-8 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 bg-primary rounded-xl border border-secondary shadow-lg
+				">
 				<div className="relative">
 					<Edit
-						className="absolute right-0 text-secondary cursor-pointer"
+						className="absolute right-0 top-0 text-secondary cursor-pointer"
 						size="1.25em"
 						onClick={editJobDetail}
 					/>
-					<h2 className="font-display text-6xl w-full text-center text-accent">
+					<h2 className="font-display text-6xl w-full text-center pt-10 text-accent">
 						{jobs[0].role}
 					</h2>
 					<ul className="text-center mt-12">
