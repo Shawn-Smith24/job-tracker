64095c8bafee4ea22c9841a1e1ab23c189c7f575
Madison Evans <m.corbinevans@gmail.com>
2023-03-28 20:28:47 -0700

    working on modal

--- client/src/Components/Modal.jsx
+++ client/src/Components/Modal.jsx
@@ -28,7 +28,7 @@ const Modal = () => {
 	return (
 		<div className="absolute w-full h-full bg-[#000000D9] z-50 ">
 			<div className="w-full h-full relative">
-				<div className="text-secondary p-4 absolute rounded-xl bg-primary w-3/4 h-5/6 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
+				<div className="text-secondary flex flex-col items-center justify-center p-4 absolute rounded-xl bg-primary w-3/4 h-5/6 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
 					<form onSubmit={handleSubmit}>
 						<div className="space-y 4">
 							<select
@@ -44,7 +44,7 @@ const Modal = () => {
 								<option value="FullStack">FullStack</option>
 							</select>
 							<input
-								className="placeholder-secondary w-full p-2 border border-secondary rounded"
+								className="placeholder-secondary my-2 w-full p-2 border border-secondary rounded"
 								type="text"
 								name="input2"
 								value={formData.input2}
@@ -52,7 +52,7 @@ const Modal = () => {
 								placeholder="Input 2"
 							/>
 							<input
-								className="placeholder-secondary w-full p-2 border border-secondary rounded"
+								className="placeholder-secondary w-full p-2 border  border-secondary rounded"
 								type="text"
 								name="input3"
 								value={formData.input3}
@@ -60,7 +60,7 @@ const Modal = () => {
 								placeholder="Input 3"
 							/>
 							<input
-								className="placeholder-secondary w-full p-2 border border-secondary rounded "
+								className="placeholder-secondary w-full p-2 mt-2 border border-secondary rounded "
 								type="text"
 								name="input4"
 								value={formData.input4}
@@ -68,7 +68,7 @@ const Modal = () => {
 								placeholder="Input 4"
 							/>
 						</div>
-						<div className="flex justify-end mt-4 space-x-4">
+						<div className="flex justify-center mt-4 space-x-4 w-full">
 							<button
 								className="px-4 py-2 text-info rounded border border-info"
 								type="button"
