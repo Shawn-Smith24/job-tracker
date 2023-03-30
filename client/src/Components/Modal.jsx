c36db487aa8e06d24474fab92b08291bbe59c8de
Madison Evans <m.corbinevans@gmail.com>
2023-03-30 08:09:11 -0700

    fixing modal

--- client/src/Components/Modal.jsx
+++ client/src/Components/Modal.jsx
@@ -28,7 +28,7 @@ const Modal = () => {
 	return (
 		<div className="absolute w-full h-full bg-[#000000D9] z-50 ">
 			<div className="w-full h-full relative">
-				<div className="text-secondary flex flex-col items-center justify-center p-4 absolute rounded-xl bg-primary w-3/4 h-5/6 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
+				<div className="text-secondary h-fit flex flex-col items-center justify-center px-4 py-10 absolute rounded-xl bg-primary w-3/4 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
 					<form onSubmit={handleSubmit}>
 						<div className="space-y 4">
 							{/* TODO: enter company name */}
@@ -125,7 +125,9 @@ const Modal = () => {
 								Cancel
 							</button>
 							<button
-								className="px-4 py-2 bg-accent text-info rounded"
+								className="
+								px-4 py-2 bg-info text-primary rounded transition
+								hover:bg-accent hover:text-info"
 								type="submit">
 								Submit
 							</button>
