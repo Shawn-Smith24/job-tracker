55101cf06e9f79c417e4dee3032c91c2d982ce44
Madison Evans <m.corbinevans@gmail.com>
2023-03-30 14:06:03 -0700

    user auth

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response, request, abort, jsonify
+from flask import Flask, make_response, request, abort, jsonify, session
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_restful import Api, Resource
@@ -12,6 +12,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+bcrypt = Bcrypt(app)
+
+app.secret_key = b'@~xH\xf2\x10k\x07hp\x85\xa6N\xde\xd4\xcd'
 
 CORS(app)
 migrate = Migrate(app, db)
@@ -19,7 +22,6 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
-
 
 class Home(Resource):
     def get(self):
